1f0df3359433d4f6ab6a139f2484b3d69f958b2b
Adam Mihajlovic <mihajlovicadam@gmail.com>
2024-03-08 17:18:35 -0500

    backend works

---
+++ server/README.md
@@ -0,0 +1,3 @@
+# Assetto Corsa Server Backend
+This folder is hosted on the server where the assetto servers are running.
+It is simply copied to the server and run.
--- src/components/ServerTile.jsx
+++ src/components/ServerTile.jsx
@@ -8,51 +8,43 @@ const ServerTile = ({ name, port }) => {
     const [totalSlots, setTotalSlots] = useState(0);
     const [occupiedSlots, setOccupiedSlots] = useState(0);
 
-    const url = `http://74.56.22.147:${port}/JSON%7C`;
+    const url = `https://home.adammihajlovic.ca/assetto/json?port=${port}`;
     // const url = "https://api.github.com/users/github"
 
-    axios({
-        url: url,
-        method: "GET",
-        // headers: { 
-        //     // mode: 'no-cors',
-            // "Access-Control-Allow-Origin" : "*",
-        //     "Access-Control-Allow-Methods": "PUT, POST, OPTIONS, GET",
-        //     "Access-Control-Allow-Headers": "Special-Request-Header",
-        //     "Access-Control-Allow-Credentials": "true",
-        //     "Access-Control-Max-Age": "240",
-        //     "Accept": "text/html,application/json"
-        // }
-    })
-    // Handle the response from backend here
-    .then((res) => {
-        console.log(res)
-    })
-    // Catch errors if any
-    .catch((err) => {
-        console.error(err)
-    });
+    // axios({
+    //     url: url,
+    //     method: "GET",
+    //     // headers: { 
+    //     //     // mode: 'no-cors',
+    //         // "Access-Control-Allow-Origin" : "*",
+    //     //     "Access-Control-Allow-Methods": "PUT, POST, OPTIONS, GET",
+    //     //     "Access-Control-Allow-Headers": "Special-Request-Header",
+    //     //     "Access-Control-Allow-Credentials": "true",
+    //     //     "Access-Control-Max-Age": "240",
+    //     //     "Accept": "text/html,application/json"
+    //     // }
+    // })
+    // // Handle the response from backend here
+    // .then((res) => {
+    //     console.log(res)
+    // })
+    // // Catch errors if any
+    // .catch((err) => {
+    //     console.error(err)
+    // });
 
-    // let status;
-    // fetch(url,
-    //     {
-    //         mode: 'no-cors',
-    //         method: 'GET',
-    //     })
-    //     .then((res) => { 
-    //         status = res.status
-    //         console.log(status)
-    //         console.log(res)
-    //         return res.json() 
-    //     })
-    //     .then((jsonResponse) => {
-    //         console.log(jsonResponse)
-    //         console.log(status)
-    //     })
-    //     .catch((err) => {
-    //         // handle error
-    //         console.error(err)
-    //     });
+    let status;
+    fetch(url)
+        .then((res) => { 
+            return res.json() 
+        })
+        .then((jsonResponse) => {
+            console.log(jsonResponse)
+        })
+        .catch((err) => {
+            // handle error
+            console.error(err)
+        });
 
     return (
         <Row className='rounded text-white'>
