69ff1aa9e3e4dcdf837e6460710daf62397efb41
Adam Mihajlovic <mihajlovicadam@gmail.com>
2024-03-10 15:53:42 -0400

    minor fix for cors + changed app name

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const app = express()
 const listenningPort = 3001
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000")
+  res.header('Access-Control-Allow-Origin', "*")
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Methods', 'DELETE, PUT, POST, GET, OPTIONS, HEAD')
   res.header(
