06610beaacf917a3578cd5195d1ea2c62736d320
Adam Mihajlovic <mihajlovicadam@gmail.com>
2024-05-22 16:10:56 -0400

    added real light and dark theme + fixed weird background on disabled ghost buttons

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -15,8 +15,28 @@ module.exports = {
   },
   daisyui: {
     themes: [
-      "light",
-      "dark",
+      {
+        light: {
+          "primary": "#2f27ce",
+          "secondary": "#dddbff",
+          "accent": "#443dff",
+          "neutral": "#040316",
+          "base-100": "#fbfbfe",
+        },
+        dark: {
+          "primary": "#3a31d8",
+          "secondary": "#020024",
+          "accent": "#0600c2",
+          "neutral": "#eae9fc",
+          "base-100": "#131315",
+        },
+        daisyLight: {
+          ...require("daisyui/src/theming/themes")["light"]
+        },
+        daisyDark: {
+          ...require("daisyui/src/theming/themes")["dark"]
+        },
+      },
       "cupcake",
       "bumblebee",
       "emerald",
